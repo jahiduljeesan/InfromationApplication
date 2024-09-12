8c4832c4d00832c52725de3c646743b365c5abc5
NightmareJahid <jahiduljeesan@gmail.com>
2024-09-12 19:25:50 +0600

    with some error

--- app/src/main/java/com/example/rakibapplication/ui/fragments/HomeFragment.java
+++ app/src/main/java/com/example/rakibapplication/ui/fragments/HomeFragment.java
@@ -5,12 +5,16 @@ import android.os.Bundle;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
+import androidx.appcompat.app.ActionBarDrawerToggle;
+import androidx.drawerlayout.widget.DrawerLayout;
 import androidx.fragment.app.Fragment;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
 
+import com.example.rakibapplication.R;
 import com.example.rakibapplication.databinding.FragmentHomeBinding;
 import com.example.rakibapplication.ui.activity.DataAddActivity;
 
@@ -18,6 +22,7 @@ import com.example.rakibapplication.ui.activity.DataAddActivity;
 public class HomeFragment extends Fragment {
 
    FragmentHomeBinding fragHome;
+   ActionBarDrawerToggle drawerToggle;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -30,18 +35,29 @@ public class HomeFragment extends Fragment {
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         fragHome = FragmentHomeBinding.inflate(inflater);
-        return fragHome.getRoot();
+        return fragHome.homeViewContent.getRoot();
     }
     @Override
     public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
-        fragHome.marqueeText.setSelected(true);
 
-        fragHome.dataAddButton.setOnClickListener(new View.OnClickListener() {
+        fragHome.homeViewContent.marqueeText.setSelected(true);
+
+        fragHome.homeViewContent.btnMenu.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                drawerToggle = new ActionBarDrawerToggle(requireActivity(), fragHome.drawerLayout ,R.string.nav_open,R.string.nav_close);
+                fragHome.drawerLayout.addDrawerListener(drawerToggle);
+                drawerToggle.syncState();
+            }
+        });
+
+        fragHome.homeViewContent.dataAddButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
               startActivity(new Intent(requireContext(), DataAddActivity.class));
             }
         });
+
     }
 }
