cc4961d715ac11fa1cb7127e5f230febc14df7dc
NightmareJahid <jahiduljeesan@gmail.com>
2024-09-12 23:49:45 +0600

    added navigation menu

--- app/src/main/java/com/example/rakibapplication/ui/fragments/HomeFragment.java
+++ app/src/main/java/com/example/rakibapplication/ui/fragments/HomeFragment.java
@@ -6,6 +6,7 @@ import android.os.Bundle;
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.ActionBarDrawerToggle;
+import androidx.core.view.GravityCompat;
 import androidx.drawerlayout.widget.DrawerLayout;
 import androidx.fragment.app.Fragment;
 
@@ -23,7 +24,7 @@ public class HomeFragment extends Fragment {
 
    FragmentHomeBinding fragHome;
    ActionBarDrawerToggle drawerToggle;
-
+   DrawerLayout drawerLayout;
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -34,21 +35,29 @@ public class HomeFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        fragHome = FragmentHomeBinding.inflate(inflater);
-        return fragHome.homeViewContent.getRoot();
+        fragHome = FragmentHomeBinding.inflate(inflater,container,false);
+        drawerLayout = fragHome.drawerLayout;
+        return fragHome.getRoot();
+
     }
     @Override
     public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
 
-        fragHome.homeViewContent.marqueeText.setSelected(true);
+        drawerToggle = new ActionBarDrawerToggle(requireActivity(), fragHome.drawerLayout ,R.string.nav_open,R.string.nav_close);
+        fragHome.drawerLayout.addDrawerListener(drawerToggle);
+        drawerToggle.syncState();
+
+         fragHome.homeViewContent.marqueeText.setSelected(true);
 
         fragHome.homeViewContent.btnMenu.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                drawerToggle = new ActionBarDrawerToggle(requireActivity(), fragHome.drawerLayout ,R.string.nav_open,R.string.nav_close);
-                fragHome.drawerLayout.addDrawerListener(drawerToggle);
-                drawerToggle.syncState();
+                if (!drawerLayout.isDrawerOpen(GravityCompat.START)) {
+                    drawerLayout.openDrawer(GravityCompat.START);
+                } else {
+                    drawerLayout.closeDrawer(GravityCompat.START);
+                }
             }
         });
 
